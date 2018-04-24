a2c33c3078b70ce8d668f93edb34221ae8e67022
6V1QZL2-PC\6V1QZL2 <thuongtxt@gmail.com>
2018-04-24 15:17:08 +0700

    Add Models

---
+++ Site.Model/Models/Footer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Site.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string  ID { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
---
+++ Site.Model/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using Site.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Site.Model.Models
+{
+    [Table("Products")]
+    public class Product : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public string CatogoryID { get; set; }
+        public string Image { get; set; }
+        public XElement MoreImage { get; set; }
+        public decimal Price { get; set; }
+        public decimal? PromotionPrice { get; set; }
+        public int? Warranty { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+        [ForeignKey("CatogoryID")]
+        public virtual ProductCategory ProductCatogories { get; set; }
+    }
+}
